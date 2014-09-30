b3d8ff18edb2d62fa07c37daf77f114afcd9af78
root <root@mirroir>
2014-09-30 19:17:35 +0200

    update miror directory detection

--- debian-ubuntu.sh
+++ debian-ubuntu.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 MIRRORDIR="$(dirname $0)/debian-ubuntu"
+[[ $MIRRORDIR == /* ]] || MIRRORDIR=$(pwd)/$MIRRORDIR
 
 sed -i "s:^set base_path.*$:set base_path\t$MIRRORDIR:" $MIRRORDIR/debian-ubuntu.mirror.list
 echo "Apt-mirror ..."
